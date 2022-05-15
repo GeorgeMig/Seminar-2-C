9ee2fa0addb554306a35f3dc1dbafe34333de974
George_M <msgoha555@yandex.ru>
2022-05-15 20:42:59 +0300

    Homework_15 completed

---
+++ Homework_15/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine ("Эта программа позволяет определить совпадает ли введенное Вами число с порядком выходного дня на неделе.");
+Console.Write ("Введите число от 1 до 7 => ");
+int num = int.Parse(Console.ReadLine());
+Console.Write( num == 7 || num == 6 ? "да " : "нет");
