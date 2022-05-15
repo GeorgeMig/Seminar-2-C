120fec94c1a42a2be6cde2de9b2f1525042a89ea
George_M <msgoha555@yandex.ru>
2022-05-15 18:13:01 +0300

    Task_16 ver. 1.0

---
+++ Task_16/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Настоящая программа способна определить, является ли одно число квадратом другого.");
+Console.Write("Введите любое положительное число =>" + " ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите ёщё одно число =>" + " ");
+int num2 = int.Parse(Console.ReadLine());
+//Console.WriteLine(num1 / num2 == num1 | num2  ? "да" : "нет");
+int minnum = 0;
+int maxnum = 0;
+if (num1 < num2)
+{
+    minnum = num1;
+    maxnum = num2;
+}
+else 
+{
+   minnum = num2;
+   maxnum = num1;
+}
+
+if (minnum * minnum == maxnum)
+{
+    Console.WriteLine("Да, является");
+}
+else
+{
+    Console.WriteLine("Нет, не является");
+}
