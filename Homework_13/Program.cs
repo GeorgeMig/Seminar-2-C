c6ec3e0f1d4f496a46c60d4fefc28cd2dafbb526
George_M <msgoha555@yandex.ru>
2022-05-15 20:25:55 +0300

    Homework_13 ver. 1.0.

---
+++ Homework_13/Program.cs
@@ -0,0 +1,19 @@
+﻿// ПРОГРАММА, КОТОРАЯ ВЫВОДИТ СЛУЧАЙНОЕ  ЧИСЛО И ОПРЕДЕЛЯЕТ ТРЕТЬЮ ЦИФРУ ЭТОГО ЧИСЛА
+
+// 
+// Решение при помощи метода (функции). (НЕ УНЕВИРСАЛЬНЫЙ. НУЖНО ДОДУМАТЬ)
+
+int number = new Random().Next(10, 1000);
+if (number > 99)
+    {
+        int thirdDigit = number % 10;
+        Console.Write($"{number} => {thirdDigit}");
+
+    }
+    else
+    {
+        int thirdDigit = (number * 10) % 10;
+        Console.Write($"{number} => третьей цифры нет");
+    }
+
+
