8a74af86e8dc573e9c045c78562798e4e0b51fa3
George_M <msgoha555@yandex.ru>
2022-05-15 19:03:25 +0300

    Homework_10 completed

---
+++ Homework_10/Program.cs
@@ -0,0 +1,22 @@
+﻿// ПРОГРАММА, КОТОРАЯ ВЫВОДИТ СЛУЧАЙНОЕ ТРЁХЗНАЧНОЕ ЧИСЛО И ОПРЕДЕЛЯЕТ ВТОРУЮ ЦИФРУ ЭТОГО ЧИСЛА.
+
+// Альтернативный алгоритм
+//int num = new Random().Next(99, 1000);
+//int firstDigit = num / 10;
+//int secondDigit = firstDigit % 10;
+//Console.Write ($"{num} => {secondDigit}");
+
+
+// Решение при помощи метода (функции).
+
+int num = new Random().Next(99, 1000);
+int res = FindSecondDig (num);
+Console.Write ($"{num} => {res}");
+
+
+int FindSecondDig(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = firstDigit % 10;
+    return secondDigit;
+}
